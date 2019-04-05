ce5ac01fa933d153bcb769f7fe9d417a877054fa
pramadito <pramadito@gmail.com>
2019-04-06 02:22:28 +0700

    final dari conversi image to rgb to cmyk

--- Session_04 Image to RGB CSV/Pixel.java
+++ Session_04 Image to RGB CSV/Pixel.java
@@ -20,7 +20,11 @@ class Pixel {
          width = image.getWidth();
          height = image.getHeight();
          
-         int count = 0;
+         
+         double cf = 0;
+         double mf = 0;
+         double yk = 0;
+
 
          try (PrintWriter writer = new PrintWriter(new File("OutputImageRGB.csv"))) {
          StringBuilder sb = new StringBuilder();
@@ -33,7 +37,7 @@ class Pixel {
 
                   sb.append("[" + c.getRed() + "." + c.getGreen() + "." + c.getBlue()+ "]");
 
-                  System.out.println("S.height: " + i + " S.width:"+ j + " Red: " + c.getRed() +"  Green: " + c.getGreen() + " Blue: " + c.getBlue());
+                  
                   
                   sb.append(',');
                      
@@ -41,14 +45,67 @@ class Pixel {
                sb.append('\n');
             }
             writer.write(sb.toString());
-            System.out.println("done!");
+            System.out.println("Done RGB!");
+         } catch (FileNotFoundException e) {
+                     System.out.println(e.getMessage());
+
+         }
+
+
+         //for cymk
+
+         try (PrintWriter writer = new PrintWriter(new File("OutputImageCMYK.csv"))) {
+         StringBuilder sb = new StringBuilder();
+            for(int i=0; i<height; i++) {
+            
+               for(int j=0; j<width; j++) {
+               
+                  
+                  Color c = new Color(image.getRGB(j, i));
+
+                  double red = c.getRed() ;
+                  double green = c.getGreen();
+                  double blue = c.getBlue();
+                  double red1 = red / 255;
+                  double green1 = green / 255;
+                  double blue1 = blue / 255;
+                  double max = (Math.max(Math.max(red1, green1), blue1));
+                  double K = 1 - max;
+                  double C = (1 - red1 - K) / (1 - K);
+                  double M = (1 - green1 - K) / (1 - K);
+                  double Y = (1 - blue1 - K) / (1 - K);
+                  double CMYK[] = {C, M, Y, K};
+                  String cmyk = "[" + Math.round(C*100.0)/100.0 + " . " + Math.round(M *100.0)/100.0 + " . " + Math.round(Y*100.0)/100.0 + " . " + Math.round(K*100.0)/100.0 + "]";
+
+
+                  sb.append(cmyk);
+                  sb.append(',');
+                     
+               }
+               sb.append('\n');
+            }
+            writer.write(sb.toString());
+            System.out.println("Done CMYK!");
          } catch (FileNotFoundException e) {
                      System.out.println(e.getMessage());
          }
+
+
+
+
+
+
       } catch (Exception e) {}
    }
    
 
+
+
+    
+    
+    
+    
+   
 
 
 
